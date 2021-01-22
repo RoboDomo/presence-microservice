694eb550f726f8a0dba53528f6808f86d4aa4557
Michael Schwartz <mykesx@gmail.com>
2021-01-22 12:05:44 -0800

    works in docker, works

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 process.title = process.env.TITLE || "presence-microservice";
-// process.env.DEBUG = "PresenceHost,HostBase";
+process.env.DEBUG = "PresenceHost,HostBase";
 
 const debug = require("debug")("PresenceHost"),
   console = require("console"),
   superagent = require("superagent"),
   HostBase = require("microservice-core/HostBase");
 
-const POLL_TIME = 3 * 1000,
-  TIMEOUT = 3 * 1000;
+const POLL_TIME = 5 * 1000,
+  TIMEOUT = 15 * 1000;
 
 const TOPIC_ROOT = process.env.TOPIC_ROOT || "presence",
   MQTT_HOST = process.env.MQTT_HOST;
 
-class Presence extends HostBase {
+let busy = false;
+
+class PresenceHost extends HostBase {
   constructor(presence) {
     super(MQTT_HOST, TOPIC_ROOT + "/" + presence.person);
     this.presence = presence;
@@ -30,34 +32,60 @@ class Presence extends HostBase {
     this.poll();
   }
 
-  async poll() {
+  async pollOne() {
     const s = {},
       device = this.device;
 
     for (;;) {
       try {
+        while (busy) {
+          await this.wait(100);
+        }
+        busy = true;
+        console.log(new Date().toLocaleTimeString(), "polling", this.device);
         const result = await superagent
           .get(`http://${this.device}`)
           .timeout(TIMEOUT);
+
+        busy = false;
         console.log("result", result);
       } catch (e) {
-        // console.log(this.device, e.code);
-        if (e.code === "ECONNREFUSED") {
+        busy = false;
+        debug(this.device, e.code);
+//        if (e.code === "ECONNABORTED") {
+//          await this.wait(1000);
+//          continue;
+//                  console.log(new Date().toLocaleTimeString(), this.device, "aborted");
+//      } 
+       if (e.code === "ECONNREFUSED" || e.code === "ECONNABORTED") {
           if (this.state.person !== true) {
             debug(this.device, this.person, "PRESENT!");
           }
           s[device] = true;
           this.state = s;
+          return;
         } else {
           if (this.state.person !== false) {
             debug(this.device, this.person, "AWAY!");
           }
           s[device] = false;
           this.state = s;
+          return;
         }
       }
+    }
+  }
+
+  async poll() {
+    for (;;) {
+      await this.pollOne();
+      console.log("poll completed");
       await this.wait(POLL_TIME);
     }
+  }
+
+  async command() {
+    return false;
   }
 }
 
@@ -66,7 +94,18 @@ const main = async () => {
   const Config = await HostBase.config();
   console.log("Config", Config.presence);
   for (const presence of Config.presence) {
-    people[presence.device] = new Presence(presence);
+    const devices = Array.isArray(presence.device)
+      ? presence.device
+      : [presence.device];
+    for (const device of devices) {
+      //      if (presence.person.toLowerCase() !== "chris") {
+      //        continue;
+      //      }
+      people[device] = new PresenceHost({
+        person: presence.person,
+        device: device,
+      });
+    }
   }
 };
 
